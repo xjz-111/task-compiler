2587b4616009ab1ceea6abe1d9325304fbc64125
Leslie <mr.feeling.heart@gmail.com>
2021-07-05 19:55:02 +0800

    init

---
+++ app/src/main/java/com/leslie/compiler/MainActivity.java
@@ -0,0 +1,17 @@
+package com.leslie.compiler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.leslie.annotation.Task;
+
+@Task
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ task-compiler/src/main/java/com/leslie/task_compiler/MyClass.java
@@ -0,0 +1,4 @@
+package com.leslie.task_compiler;
+
+public class MyClass {
+}
